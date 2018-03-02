1aa9ace010213865466742913b85d5143b3370e3
Patrick <mussi_kat@hotmail.com>
2018-03-02 11:30:42 +0100

    Timer på predifineret

--- app/src/main/java/com/example/nicolai/diceintent/BEDiceHistory.java
+++ app/src/main/java/com/example/nicolai/diceintent/BEDiceHistory.java
@@ -5,15 +5,19 @@ package com.example.nicolai.diceintent;
  */
 
 import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BEDiceHistory implements Serializable{
 
     String result;
-
+    Date currentTime = Calendar.getInstance().getTime();
     public BEDiceHistory(String diceResult)
     {
         result = diceResult;
+        currentTime.toString();
+
     }
 
     public String getResult() { return result; }
--- app/src/main/java/com/example/nicolai/diceintent/Die.java
+++ app/src/main/java/com/example/nicolai/diceintent/Die.java
@@ -4,19 +4,26 @@ package com.example.nicolai.diceintent;
  * Created by Nicolai on 01-03-2018.
  */
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Die {
+    Calendar c = Calendar.getInstance();
+    SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+    String formattedDate = df.format(c.getTime());
 
     ArrayList<BEDiceHistory> die;
 
     public Die()
     {
         die = new ArrayList<BEDiceHistory>();
-        die.add(new BEDiceHistory("3 + 1"));
-        die.add(new BEDiceHistory("6 + 6"));
-        die.add(new BEDiceHistory("2 + 4"));
-        die.add(new BEDiceHistory("5 + 2"));
+        die.add(new BEDiceHistory("3 + 1"+ "  " + formattedDate ));
+        die.add(new BEDiceHistory("6 + 6"+ "  " + formattedDate));
+        die.add(new BEDiceHistory("2 + 4"+ "  " + formattedDate));
+        die.add(new BEDiceHistory("5 + 2"+ "  " + formattedDate));
+
     }
 
     public ArrayList<BEDiceHistory> getAll()
